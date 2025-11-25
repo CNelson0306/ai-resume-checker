5f5cff2f38e8fbd3737bfb5f8e512db4f74faf68
Carl Nelson <99733202+CNelson0306@users.noreply.github.com>
2025-11-25 10:45:58 +0000

    Update

--- src/aws-export.js
+++ src/aws-export.js
@@ -1,8 +1,8 @@
 const awsConfig = {
   Auth: {
-    region: process.env.NEXT_REGION,
-    userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID,
-    userPoolWebClientId: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID,
+    region: process.env.NEXT_REGION || "",
+    userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID || "",
+    userPoolWebClientId: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID || "",
     mandatorySignIn: false,
     authenticationFlowType: "USER_PASSWORD_AUTH",
   },
